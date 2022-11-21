8852d0364627f26812657775ff7ea8a5d5b0f3d8
whddla <whddla8912@gmail.com>
2022-11-21 21:49:24 +0900

    메인 로고, 챗봇 이미지 변경
    메인 로고, 챗봇 이미지 변경

--- sota/sota/views.py
+++ sota/sota/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render,redirect
-from sota.models import User
+from sota.models import User,LProduct,Deposit,Card,Transation
 def index(request):
     try: 
         user = User.objects.get(idx= int(request.session['login']))
     except:
         return redirect('/member/login')
+    lp = LProduct.objects.filter(user_idx=user.idx)
+    dp = Deposit.objects.filter(user_idx=user.idx)
+    c = Card.objects.filter(user_idx=user.idx)
+    trans = []
+    for i in lp:
+        ac = i.account
+        trans.append(Transation.objects.filter(user_idx=user.idx, account=ac).last())
     context = {
         'user':user,
+        'lp':lp,
+        'dp':dp,
+        'c':c,
+        'trans':trans
     }
     return render(request, 'index.html',context)
